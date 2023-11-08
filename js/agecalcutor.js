502682298194aff105c439af71708d760bd6b5c7
Hassandurra <Hassan2109f@aptechgdn.net>
2023-11-09 01:04:39 +0500

    cahnges in calculater js

--- js/agecalcutor.js
+++ js/agecalcutor.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
    let ageContainer     = $('#ageInput');
    let ageInput         = $('input[name="age"]');
    let submitButton     = $("#submitButton");
-
+   
     $(submitButton).on('click' , function(e){
         e.preventDefault();
         isValid = true ;
@@ -24,15 +24,18 @@ $(document).ready(function(){
             ageContainer.fadeOut();
 
         } 
+        if(dateOfBirthInput.val() == new Date()){
+
+        }
         if(isValid){
             if(selectionType.val() == "year")
             {
                 console.log("Year only");
                 var age = year_calculate(dateOfBirthInput.val()) ;
                 if(age == "months" )
-                {
+                {   selectionType.val("month");
                     age = months_calculate(dateOfBirthInput.val());
-                    ageInput.val(age);
+                    ageInput.val(age + " " + "Months");
                     ageContainer.show();                    
                     if(age == "error"){
                         e.preventDefault();
@@ -40,11 +43,12 @@ $(document).ready(function(){
                         toastr["error"]("Date Of Birth And Current Date cannot be same");
                         return false;
                     }
+                
                 }
                 else
                 {
                     ageContainer.show();
-                    ageInput.val(age);
+                    ageInput.val(age + " " + "Years");
                 }
             }
             else if(selectionType.val() == "month")
@@ -52,26 +56,51 @@ $(document).ready(function(){
                 var months = months_calculate(dateOfBirthInput.val());
                 if(months == "error"){
                     e.preventDefault();
+                    ageContainer.fadeOut();
                     toastr["error"]("Date Of Birth And Current Date cannot be same");
                     return false;
                 }
+           
                 ageContainer.show();
                 ageInput.val(months);
             }
             else{
-                ageContainer.show();
-                ageInput.val("1 Years and 1 Month");
+                var totalAge = years_months(dateOfBirthInput.val());
+                if(totalAge == "error"){
+                    e.preventDefault();
+                    ageContainer.fadeOut();
+                    toastr["error"]("Date Of Birth And Current Date cannot be same");
+                    return false;
+                }
+                ageContainer.show();    
+                ageInput.val(totalAge);
+                
             }
         }
     })
 
+    function years_months(birthDate){
+        var totalYears =  year_calculate(birthDate);
+        var totalMonths = months_calculate(birthDate);
+        if(totalYears == "months" ){
+            return totalMonths + " "+ "Months" ;
+        }
+        if(totalMonths == "error")
+        {
+            return "error";
+        }
+        if(totalMonths == "greater"){
+            return "greater";
+        }
+        return totalYears + " " + "Years" + " " + "and" + " " + totalMonths + " " + "Months";
+     }
     function year_calculate(birthDate){
         var dateOfBirth  = new Date(birthDate);
         var currentdate  = new Date();
         var years        = currentdate.getFullYear() - dateOfBirth.getFullYear();
         var months       = (dateOfBirth.getMonth() - currentdate.getMonth() + 12) % 12 ;
         if(currentdate.getFullYear() != dateOfBirth.getFullYear()){
-            return years + " " + "Years";
+            return years;
         }
         else{
             return "months";
@@ -89,14 +118,17 @@ $(document).ready(function(){
         }
         
         var totalMonths = yearsDiff * 12 + monthsDiff;
-        if( currentdate.getMonth() === dateOfBirth.getMonth() && currentdate.getFullYear() == dateOfBirth.getFullYear() && currentdate.getDate() == dateOfBirth.getDate()){
+        var totalEqual   =  currentdate.getMonth() === dateOfBirth.getMonth() && currentdate.getFullYear() == dateOfBirth.getFullYear() && currentdate.getDate() == dateOfBirth.getDate();
+        var totalGreater =  currentdate.getMonth() >= dateOfBirth.getMonth() && currentdate.getFullYear() >= dateOfBirth.getFullYear() && currentdate.getDate() > dateOfBirth.getDate()
+        if(totalEqual){
             return "error";
         }   
-         
-        return totalMonths + " " + "Months";
+      
+        return totalMonths;
     }
+    function days_calculate(birthDate){
 
-
+    }
 
 
    
